6a1838984674fbdfea3767d8d00089a571483c1c
Spinaker19 <80647693+Spinaker19@users.noreply.github.com>
2021-06-30 14:40:52 +0200

    Add files via upload

---
+++ Trone.py
@@ -0,0 +1,87 @@
+import RPi.GPIO as GPIO
+import cv2
+from PIL import Image
+from pygame import mixer
+
+blue_button = 1
+green_button = 2
+red_button = 3
+white_button = 4 
+yellow_button = 5 
+buzzer = 6
+led = 7
+
+
+
+def read_vid(Source, frameRate):
+    path = "C:/Programation/Trone/Brouillon/Source/"
+    file_name = path + Source
+    window_name = "window"
+    interframe_wait_ms = frameRate
+
+    cap = cv2.VideoCapture(file_name)
+    cap.open(file_name)
+    if not cap.isOpened():
+        print("Error: Could not open video.")
+        exit()
+
+    cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    while (True):
+        ret, frame = cap.read()
+        if not ret:
+            print("Reached end of video, exiting.")
+            break
+
+        cv2.imshow(window_name, frame)
+        if cv2.waitKey(interframe_wait_ms) & 0x7F == ord('q'):
+            print("Exit requested.")
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def background(source):
+    path = "C:/Programation/Trone/Brouillon/Source/Test/"
+    file_name = path + source
+    print(file_name)
+    img = Image.open(file_name)
+    img.show()
+
+def play_sound(source):
+    path = "C:/Programation/Trone/Brouillon/Source/Sono/"
+    file_name = path + source
+    mixer.init()
+    sound = mixer.Sound(file_name)
+    sound.play()
+    print("Son joué")
+
+
+def mainloop():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(blue_button, GPIO.IN)
+    GPIO.setup(green_button, GPIO.IN)
+    GPIO.setup(red_button, GPIO.IN)
+    GPIO.setup(white_button, GPIO.IN)
+    GPIO.setup(yellow_button, GPIO.IN)
+    GPIO.setup(buzzer, GPIO.IN)
+    GPIO.setup(led, GPIO.OUT)
+
+    while True:
+        if GPIO.input (blue_button) :
+            read_vid("TestBlanc", 25)
+        elif GPIO.input (green_button) :
+            read_vid("TestBleue", 25)
+        elif GPIO.input (red_button) :
+            read_vid("TestJaune", 25)
+        elif GPIO.input (white_button) :
+            read_vid("TestRouge", 25)
+        elif GPIO.input (yellow_button) :
+            read_vid("TestVerte", 25)
+        elif GPIO.input (buzzer) :
+            play_sound("Alarm_boat.mp3")
+            read_vid("TestBuzzer", 25)
+
+background("Background.jpg")
+mainloop()
